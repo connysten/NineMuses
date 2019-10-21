e4976bf380966ab45fd9ecf1b40d43333ead9058
Jonathan <jonthehellgren@hotmail.se>
2019-10-21 12:37:08 +0200

    Fixade lite mer på Layout sidan samt gjorde vyn för index sidan

--- NineMuses/App_Start/BundleConfig.cs
+++ NineMuses/App_Start/BundleConfig.cs
@@ -23,8 +23,10 @@ namespace NineMuses
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/icons.css",
+                      "~/Content/Layout.css",
+                      "~/Content/index.css",
+                      "~/Content/assets.css"));
         }
     }
 }
