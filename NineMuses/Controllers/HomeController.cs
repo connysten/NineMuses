250580a4d5bd5598d9bdc6a3dc69896204f9a1c7
Jonathan <jonthehellgren@hotmail.se>
2019-10-22 12:26:30 +0200

    NU ska LoginBranch vara mergat med master

--- NineMuses/Controllers/HomeController.cs
+++ NineMuses/Controllers/HomeController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
+using NineMuses.Models; 
 
 namespace NineMuses.Controllers
 {
@@ -11,6 +15,51 @@ namespace NineMuses.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult UserProfile(string UserID)
+        {
+            if (UserID == null || Session["UserID"].ToString() != UserID)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            using (SqlCommand command = new SqlCommand("spGetUser", conn))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add("@Responsemessage", SqlDbType.Int).Direction = ParameterDirection.Output;
+                command.Parameters.AddWithValue("@UserID", UserID);
+                command.Parameters.Add("@Username", SqlDbType.VarChar, 50).Direction = ParameterDirection.Output;
+
+                conn.Open();
+
+                command.ExecuteNonQuery();
+                if (Convert.ToInt32(command.Parameters["@Responsemessage"].Value) == 1)
+                {
+                    var User = new UserModel
+                    {
+                        Username = (string)command.Parameters["@Username"].Value
+                    };
+                    return View(User);
+                }
+                //SqlDataReader reader = command.ExecuteReader();
+
+                //if (Convert.ToInt32(command.Parameters["@Responsemessage"].Value) == 1)
+                //{
+                //    while (reader.Read())
+                //    {
+                //        var User = new UserModel
+                //        {
+                //            Username = (string)reader["Username"]
+                //        };
+
+                //    }
+                //    return View(User);
+                //}
+                conn.Close();
+            }
+
+            return RedirectToAction("Index", "Login");
         }
 
         public ActionResult Video()
---
+++ NineMuses/Controllers/LoginController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using NineMuses.Models;
+using System.Data;
+using System.Globalization;
+
+namespace NineMuses.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            if (Session["UserID"] == null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("UserProfile", "Home", new { UserID = Session["UserID"].ToString() });
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(UserModel m)
+        {
+            using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            using (SqlCommand command = new SqlCommand("spValidateUser", conn))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@Username", m.Username);
+                command.Parameters.AddWithValue("@Password", m.Password);
+                command.Parameters.Add("@Responsemessage", SqlDbType.Int).Direction = ParameterDirection.Output;
+                command.Parameters.Add("@UserID", SqlDbType.Int).Direction = ParameterDirection.Output;
+
+                conn.Open();
+                command.ExecuteNonQuery();
+
+                if (Convert.ToInt32(command.Parameters["@Responsemessage"].Value) == 1)
+                {
+                    Session["UserID"] = Convert.ToInt32(command.Parameters["@UserID"].Value);
+                    return RedirectToAction("UserProfile", "Home", new { UserID = Session["UserID"].ToString() });
+                }
+            }
+
+
+
+            ModelState.AddModelError("", "Incorrect Login");
+            ModelState.SetModelValue("Password", new ValueProviderResult(string.Empty, string.Empty, CultureInfo.InvariantCulture));
+
+            return View(m);
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(UserModel m)
+        {
+            using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            using (SqlCommand command = new SqlCommand("spNewUser", conn))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@Username", m.Username);
+                command.Parameters.AddWithValue("@Password", m.Password);
+                command.Parameters.Add("@Responsemessage", SqlDbType.Int).Direction = ParameterDirection.Output;
+                command.Parameters.Add("@UserID", SqlDbType.Int).Direction = ParameterDirection.Output;
+
+                conn.Open();
+                command.ExecuteNonQuery();
+
+                if (Convert.ToInt32(command.Parameters["@Responsemessage"].Value) == 1)
+                {
+                    Session["UserID"] = Convert.ToInt32(command.Parameters["@UserID"].Value);
+                    return RedirectToAction("UserProfile", "Home", new { UserID = Session["UserID"] });
+                }
+            }
+
+            return View(m);
+        }
+    }
+}
