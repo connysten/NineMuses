5e4483e2ba193023d66e283b5a4c4d2795280505
Simon Azmoudeh Fard <simonazmoudehfard@gmail.com>
2019-10-24 15:30:09 +0200

    Added a search function

---
+++ NineMuses/Controllers/SearchController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using NineMuses.Models;
+using NineMuses.ViewModels;
+
+namespace NineMuses.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        
+        [HttpPost]
+        public ActionResult Index(string search)
+        {
+            var model = new SearchVIewModel();
+            using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString))
+            using (SqlCommand command = new SqlCommand("spVideoSearch", conn))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@SearchString", search);
+
+                conn.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    var video = new VideoModel();
+                    while (reader.Read())
+                    {
+                        video = new VideoModel()
+                        {
+                            UserID = (int)reader["UserID"],
+                            VideoID = (int)reader["VideoID"],
+                            Thumbnail = (string)reader["Thumbnail"],
+                            Source = (string)reader["Source"],
+                            Title = (string)reader["Title"],
+                            Description = (string)reader["Description"],
+                            Views = (int)reader["Views"],
+                            UploadDate = (DateTime)reader["UploadDate"]
+                        };
+                        model.Videos.Add(video);
+                    }
+
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ NineMuses/ViewModels/SearchVIewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NineMuses.Models;
+
+namespace NineMuses.ViewModels
+{
+    public class SearchVIewModel
+    {
+        public List<VideoModel> Videos { get; set; } = new List<VideoModel>();
+    }
+}
