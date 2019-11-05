810175b1f464d0cda3e06f49422c25c8d5356ebc
Jonathan <jonthehellgren@hotmail.se>
2019-11-05 18:57:51 +0100

    Stylades om lite och fixade på indexsidan

--- NineMuses/App_Start/BundleConfig.cs
+++ NineMuses/App_Start/BundleConfig.cs
@@ -31,7 +31,7 @@ namespace NineMuses
                       "~/Content/Layout.css",
                       "~/Content/index.css",
                       "~/Content/assets.css",
-                      "~/Content/video.css",
+                      "~/Content/view.css",
                       "~/Content/sign-in-sign.up.css",
                       "~/Content/search.css",
                       "~/Content/upload.css",
